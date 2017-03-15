4c764bd8cd30c569cc666b86615dcf3f7e056900
nage-pi <nagen@RasPi>
2017-03-15 04:22:03 +0000

    a	modified:   robot-motor-test.py add forward and reverse functions

--- robot-motor-test.py
+++ robot-motor-test.py
@@ -1,16 +1,33 @@
 import RPi.GPIO as gpio
 import time
 
-gpio.setmode(gpio.BOARD)
-'''gpio.setup(7, gpio.OUT)
-gpio.setup(11, gpio.OUT)'''
-gpio.setup(13, gpio.OUT)
-gpio.setup(15, gpio.OUT)
+'''Initialize and setup GPIO ports open and ready'''
+def init():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(7, gpio.OUT)
+    gpio.setup(11, gpio.OUT)
+    gpio.setup(13, gpio.OUT)
+    gpio.setup(15, gpio.OUT)
 
-'''gpio.output(7, True)
-gpio.output(11, True)
-gpio.output(13, True)'''
-gpio.output(15, False)
-time.sleep(3)
+'''Move 4 wheels turn forward'''
+def forward(tf):
+    init()
+    gpio.output(7, False)
+    gpio.output(11, True)
+    gpio.output(13, True)
+    gpio.output(15, False)
+    time.sleep(tf)
+    gpio.cleanup()
 
-gpio.cleanup()
+'''Move 4 wheels turn backward'''
+def reverse(tf):
+    init()
+    gpio.output(7, True)
+    gpio.output(11, False)
+    gpio.output(13, False)
+    gpio.output(15, True)
+    time.sleep(tf)
+    gpio.cleanup()
+    
+forward(0.5)
+reverse(0.5)
