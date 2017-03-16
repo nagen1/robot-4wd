4342312b408965ff00d8961528504222fe2b2efc
nage-pi <nagen@RasPi>
2017-03-15 23:01:56 -0400

    Changes to be committed: 	new file:   robot-keyHit-move.py

---
+++ robot-keyHit-move.py
@@ -0,0 +1,107 @@
+import RPi.GPIO as gpio
+import sys
+import time
+import tkinter as tk
+
+'''Config GPIO Ports'''
+a = 7
+b = 11
+c = 13
+d = 15
+
+'''Initialize and setup GPIO ports open and ready'''
+def init():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(a, gpio.OUT)
+    gpio.setup(b, gpio.OUT)
+    gpio.setup(c, gpio.OUT)
+    gpio.setup(d, gpio.OUT)
+
+'''Move 4 wheels turn forward'''
+def forward(tf):
+    gpio.output(a, False)
+    gpio.output(b, True)
+    gpio.output(c, True)
+    gpio.output(d, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+
+'''Move 4 wheels turn backward'''
+def reverse(tf):
+    gpio.output(a, True)
+    gpio.output(b, False)
+    gpio.output(c, False)
+    gpio.output(d, True)
+    time.sleep(tf)
+    gpio.cleanup()
+
+
+'''left turn'''
+def left_turn(tf):
+    gpio.output(a, True)
+    gpio.output(b, True)
+    gpio.output(c, True)
+    gpio.output(d, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+
+'''Right turn'''
+def right_turn(tf):
+    gpio.output(a, False)
+    gpio.output(b, True)
+    gpio.output(c, False)
+    gpio.output(d, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+
+'''Pivot left turn'''
+def pivot_left(tf):
+    gpio.output(a, True)
+    gpio.output(b, False)
+    gpio.output(c, True)
+    gpio.output(d, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+
+'''Pivot right turn'''
+def pivot_right(tf):
+    gpio.output(a, False)
+    gpio.output(b, True)
+    gpio.output(c, False)
+    gpio.output(d, True)
+    time.sleep(tf)
+    gpio.cleanup()
+
+  
+'''use Keyboard to rover to move'''
+def control_rover(event):
+    init()
+    print ("Key: ", event.char)
+    key_press = event.char
+    sleep_time = 0.030
+
+    if key_press == 'w':
+        forward(sleep_time)
+    elif key_press == 's':
+        reverse(sleep_time)
+    elif key_press == 'a':
+        left_turn(sleep_time)
+    elif key_press == 'd':
+        right_turn(sleep_time)
+    elif key_press == 'f':
+        pivot_right(sleep_time)
+    elif key_press == 'l':
+        pivot_left(sleep_time)
+    else:
+        None
+
+key_press = ''
+command = tk.Tk()
+command.bind('<KeyPress>', control_rover)
+command.mainloop()
+    
+    
