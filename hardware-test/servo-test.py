a1745a7832eb70a725be0dca8bab87782e4349b3
nagen1-rpi <nagen1@rpi.com>
2017-09-13 23:46:54 -0400

    servo test complete with tkinter window

--- hardware-test/servo-test.py
+++ hardware-test/servo-test.py
@@ -1,11 +1,28 @@
 import RPi.GPIO as gpio
 import time
+from tkinter import *
 
 gpio.setmode(gpio.BOARD)
 gpio.setup(40, gpio.OUT)
 pwm = gpio.PWM(40, 100)
 
 pwm.start(5)
-pwm.ChangeDutyCycle(7.5)
+#pwm.ChangeDutyCycle(20)
 
-gpio.cleanup()
+class App:
+    def __init__(self, master):
+         frame = Frame(master)
+         frame.pack()
+         scale = Scale(frame, from_=0, to=180, orient=HORIZONTAL, command=self.update)
+         scale.grid(row=0)
+    def update(self, angle):
+         duty = float(angle) / 10.0 + 2.5
+         pwm.ChangeDutyCycle(duty)
+
+root = Tk()
+root.wm_title('Servo Control')
+app = App(root)
+root.geometry("200x50+0+0")
+root.mainloop()
+
+#gpio.cleanup()
